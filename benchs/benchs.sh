73feb79178bc86b9b755ad23b8683d8769910379
sletuffe <git@letuffe.org>
2013-01-21 00:18:36 +0100

    benchmark added some zone, and default images + timeing of generation

--- benchs/benchs.sh
+++ benchs/benchs.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-
+function time_spent 
+{
+if [ $1 == "start" ] ; then
+  start_time=`date +%s.%N`
+  else
+  {
+    time=`date +%s.%N`
+    echo $2,`echo "$time-$start_time" | bc -l`
+  }
+  fi
+}
+                
+                
+IFS=$'\n'
 for x in `cat zones | grep -v ^#` ; do
-time nik2img.py $1 -f png $2/$name.png -a none
+name=`echo $x | cut -f1 -d\;`
+bbox=`echo $x | cut -f2 -d\;`
+
+
+#I don't get why using $bbox does fail...
+bbox1=`echo $bbox | cut -f1 -d\ `
+bbox2=`echo $bbox | cut -f2 -d\ `
+bbox3=`echo $bbox | cut -f3 -d\ `
+bbox4=`echo $bbox | cut -f4 -d\ `
+
+time_spent start
+nik2img.py $1 $2/$name.png -d 1500 900 --srs 900913 --bbox $bbox1 $bbox2 $bbox3 $bbox4 -a none -f png
+time_spent stop $name
 done
